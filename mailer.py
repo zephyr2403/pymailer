4976c33c1cf6c69218953fc9ad89eefd0bc1effb
zephyr2403 <ashutosh.extrimis.2403@gmail.com>
2017-12-15 22:59:54 +0530

    Message Control Flow

--- mailer.py
+++ mailer.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 from Tkinter import *
 import time
+import threading
 import smtplib
 import math
 import inspect as iz
 import os
 import random
 
-#ser = -2
+
 def change_text(root,shw,text,ftext,iclr,fclr,spd):
     shw.config(text=text,fg=iclr);
     root.update()
@@ -15,50 +16,49 @@ def change_text(root,shw,text,ftext,iclr,fclr,spd):
     shw.config(text=ftext,fg=fclr);
     root.update()
 
-def blink(widget,iclr,fclr,spd):
-    widget.config(bg=fclr)
-    root.update()
-    time.sleep(spd)
-    widget.config(bg=iclr);
-    root.update()
-
-def animate(start,end,xrat,yrat,spd,root):
-    for i in range(start,end,4):
-        root.geometry(str(i*xrat)+"x"+str(int(math.ceil(i*yrat+i/2))))
-        root.update()
-        time.sleep(spd)
-
-def cont_server(root,shw,user,pswd,ser):
-    global conn
-    if(ser==1):
-        smtpname="smtp.gmail.com"
-    elif(ser==2):
-        smtpname="smtp.mail.yahoo.com"
-    else:
-        smtpname="smtp-mail.outlook.com"
-    try:
-        change_text(root,shw,"Connecting.","Connecting..","#00cccc","#00cccc",1)
-        conn=smtplib.SMTP(str(smtpname),587)
-        change_text(root,shw,"Connecting...","Connected","#00cccc","#66cc00",1)
-        conn.ehlo()
-        change_text(root,shw,"Connected","Starting Encrption Before Logging in","#66cc00","#00cccc",1)
-        conn.starttls()
-        change_text(root,shw,"Successfully Encrpted","Logging In..","#66cc00","#00cccc",1)
+class CONNECTION(object):
+    smtpname=""
+    user=""
+    pswd=""
+    ser=""
+    def cont_server(self,root,shw,user,pswd,ser):
+        global conn
+        if(ser==1):
+            CONNECTION.smtpname="smtp.gmail.com"
+        elif(ser==2):
+            CONNECTION.smtpname="smtp.mail.yahoo.com"
+        else:
+            CONNECTION.smtpname="smtp-mail.outlook.com"
         try:
-            conn.login(str(user),str(pswd))
-            change_text(root,shw,"Logging In....","Successfully Logged In ","#00cccc","#66cc00",1)
-            if log_type==0:
-               code4=random.randint(1000,9999)
-               print code4
-               code_handling(code4,0,user,pswd,ser)
-            message_box(root,user)
+            change_text(root,shw,"Connecting.","Connecting..","#00e6e6","#00e6e6",1)
+            conn=smtplib.SMTP(str(CONNECTION.smtpname),587)
+            change_text(root,shw,"Connecting...","Connected","#00e6e6","#66cc00",1)
+            conn.ehlo()
+            change_text(root,shw,"Connected","Starting Encrption Before Logging in","#66cc00","#00e6e6",1)
+            conn.starttls()
+            change_text(root,shw,"Successfully Encrpted","Logging In..","#66cc00","#00e6e6",1)
+            try:
+                CONNECTION.user=user
+                CONNECTION.pswd=pswd
+                conn.login(str(CONNECTION.user),str(CONNECTION.pswd))
+                change_text(root,shw,"Logging In....","Successfully Logged In ","#00e6e6","#66cc00",1)
+                if log_type==0:
+                   code4=random.randint(1000,9999)
+                   code_handling(code4,0,CONNECTION.user,CONNECTION.pswd,ser)
+                message_box(root,CONNECTION.user)
+            except:
+                change_text(root,shw,"Login Failed.","Make Sure Username and Password is Correct","#e63900","#e63900",1)
+                change_text(root,shw,"Make Sure Username and Password is Correct","Enable Less Secure Login On Your Account","#e63900",fclr,2)
+                change_text(root,shw,"Enable Less Secure Login On Your Account",ftext,"#e63900",fclr,2)
         except:
-            change_text(root,shw,"Login Failed.","Make Sure Username and Password is Correct","red","red",1)
-            change_text(root,shw,"Make Sure Username and Password is Correct","Enable Less Secure Login On Your Account","red",fclr,2)
-            change_text(root,shw,"Enable Less Secure Login On Your Account",ftext,"red",fclr,2)
-    except:
-            change_text(root,shw,"No Internet Connection.Please Try Again",ftext,"red",fclr,2)
+                change_text(root,shw,"No Internet Connection.Please Try Again",ftext,"#e63900",fclr,2)
 
+    def reconnect(self):
+        conn = smtplib.SMTP(CONNECTION.smtpname,587)
+        conn.ehlo()
+        conn.starttls()
+        conn.login(str(CONNECTION.user),str(CONNECTION.pswd))
+        return conn
 
 
 
@@ -85,7 +85,6 @@ def decrypt(use):
     return str(use)
 
 def code_handling(code4,z,*args):
-    print "exe"
     global ser
     p=os.path.dirname(iz.stack()[0][1])
     if not os.path.exists(p+r'/db'):
@@ -95,13 +94,10 @@ def code_handling(code4,z,*args):
     re=fw.read()
     fw.close()
     if(z==0):
-        print "exe"
         user = args[0]
         pswd = args[1]
         ser = args[2]
-        print user,pswd
         if re.find(encrypt(user))==-1 and re.find(encrypt(pswd))==-1:
-            print "exe1"
             while(str(code4) in re):
                 code4=random.randint(1000,999)
             fw=open(p+r'/db','a')
@@ -110,8 +106,8 @@ def code_handling(code4,z,*args):
             conn.sendmail(str(user),str(user),'Subject:Four Digit Code\n\nDear User\n\tYour Speed Code is :'+str(code4))
             z=Tk()
             z.wm_title('Speed Code Details')
-            m=Label(z,text='Your Speed Code Is: '+str(code4))
-            m.pack(padx=100)
+            m=Label(z,text='Your Speed Code Is: '+str(code4),font="Tahoma 12 bold",bg="#DFD8DC",fg="#454545")
+            m.pack(padx=100,pady=20)
     elif(z==1):
             if(re.find(str(code4))!=-1):
                 ser=re[re.find(str(code4))-1]
@@ -128,67 +124,83 @@ def codevalidate(root,shw,code4):
         try:
             code4=int(code4)
         except:
-            change_text(root,shw,"Enter Valid Number",ftext,"red",fclr,2)
+            change_text(root,shw,"Enter Valid Number",ftext,"#e63900",fclr,2)
         if code4 < 1000 or code4/10000 >0 :
-            change_text(root,shw,"Enter 4 Digit Code",ftext,"red",fclr,2)
+            change_text(root,shw,"Enter 4 Digit Code",ftext,"#e63900",fclr,2)
         else:
             user,pswd,ser=code_handling(code4,1)
             if(user == 'false'):
-                change_text(root,shw,"Code Not Found",ftext,"red",fclr,2)
+                change_text(root,shw,"Code Not Found",ftext,"#e63900",fclr,2)
             else:
                 ser=int(ser)
-                cont_server(root,shw,user,pswd,ser)
+                CONNECTION().cont_server(root,shw,user,pswd,ser)
 
 
 def validation(root,shw,user,pswd,var):
         ser=var.get()
         if ser==0:
-            change_text(root,shw,"Select Server",ftext,"red","#454545",1)
+            change_text(root,shw,"Select Server",ftext,"#e63900","#454545",1)
         elif not user:
-            change_text(root,shw,"Enter Username",ftext,"red","#454545",1)
+            change_text(root,shw,"Enter Username",ftext,"#e63900","#454545",1)
         elif not pswd:
-            change_text(root,shw,"Enter Password",ftext,"red","#454545",1)
+            change_text(root,shw,"Enter Password",ftext,"#e63900","#454545",1)
         elif user.find("@gmail.com")==-1 and user.find("@yahoo.com")==-1 and user.find("@hotmail.com")==-1:
-            change_text(root,shw,"Enter Valid Email Addresss",ftext,"red",fclr,1)
+            change_text(root,shw,"Enter Valid Email Addresss",ftext,"#e63900",fclr,1)
         elif ser == 1 and user.find("@gmail.com") == -1 or ser==2 and user.find("@yahoo.com") == -1 or ser ==3 and user.find("@outlook.com") == -1 :
-            change_text(root,shw,"Incorrect Server Selected",ftext,"red",fclr,1)
+            change_text(root,shw,"Incorrect Server Selected",ftext,"#e63900",fclr,1)
         else:
-            cont_server(root,shw,user,pswd,ser)
+            CONNECTION().cont_server(root,shw,user,pswd,ser)
 
-
-def takedet(user):
-        mess = "Subject:" + sub_ent.get("1.0","end-1c") + "\n\n" + mess_ent.get("1.0","end-1c")
-        reciv=to_ent.get()
+def takedet(master,lab,user,reciv,sub,mes):
+        mess = "Subject:" + sub + "\n\n" + mes
         try:
             conn.sendmail(str(user),str(reciv),str(mess))
+            change_text(master,lab,"Message Sent to "+str(reciv),"Enter Your Message","green","#454545",2)
         except:
-            pass
+            change_text(master,lab,"Reconnecting..","Reconnecting...","green","green",1)
+            print "--"
+            conn=CONNECTION().reconnect()
+            print "--++"
+            try:
+                print "--++--"
+                conn.sendmail(str(user),str(reciv),str(mess))
+                print "--++--++"
+                change_text(master,lab,"Message Sent to "+str(reciv),"Enter Your Message","green","#454545",2)
+            except:
+                change_text(master,lab,"Message Not Sent","Enter Your Message","#e63900","#454545",2)
 
 def message_box(root,user):
     root.destroy()
     master=Tk()
     master.wm_title("Send Message")
     master.configure(background="#DFD8DC")
-    #global to_ent,sub_ent,mess_ent
-    to=Label(master,text="To:",font="Tahoma 12 bold",bg="#DFD8DC",fg="#454545")
-    to.grid(row=0,column=0,padx=(10,0),pady=(5,20))
 
-    to_ent=Entry(master,width=66,font="Tahoma 12",relief="groove",bg="#DFD8DC",fg="#454545",insertbackground="#454545")
-    to_ent.grid(row=0,column=1,padx=(0,10),pady=(5,20))
+    lab=Label(master,text="Enter Your Message",font="Tahoma 12 bold",bg="#DFD8DC",fg="#454545")
+    lab.grid(row=0,column=0,columnspan=4,padx=(10,0),pady=(15,10))
+
+    to=Label(master,text="To:",font="Tahoma 12 bold",bg="#DFD8DC",fg="#454545")
+    to.grid(row=1,column=0,padx=(10,0),pady=(5,20))
+
+    to_ent=Entry(master,width=66,font="Tahoma 12",bg="#DFD8DC",fg="#454545",insertbackground="#454545")
+    to_ent.grid(row=1,column=1,columnspan=3,padx=(0,40),pady=(5,20))
 
     sub=Label(master,text="Subject:",font="Tahoma 12 bold",bg="#DFD8DC",fg="#454545")
-    sub.grid(row=1,column=0,padx=(10,0),pady=(5,20))
+    sub.grid(row=2,column=0,padx=(10,0),pady=(5,20))
 
-    sub_ent=Text(master,width=66,height=2,font="Tahoma 12",relief="groove",bg="#DFD8DC",fg="#454545",insertbackground="#454545")
-    sub_ent.grid(row=1,column=1,padx=(0,10),pady=(5,20))
+    sub_ent=Text(master,width=66,height=2,font="Tahoma 12",bg="#DFD8DC",fg="#454545",insertbackground="#454545")
+    sub_ent.grid(row=2,column=1,columnspan=3,padx=(0,40),pady=(5,20))
 
-    mess_ent=Text(master,width=66,height=11,font='Tahoma 12',relief="groove",bg="#DFD8DC",fg="#454545",insertbackground="#454545")
-    mess_ent.grid(row=2,column=1,padx=(0,10),pady=(5,20))
+    mess_ent=Text(master,width=66,height=11,font='Tahoma 12',bg="#DFD8DC",fg="#454545",insertbackground="#454545")
+    mess_ent.grid(row=3,column=1,columnspan=3,padx=(0,40),pady=(5,20))
 
-    mess = "Subject:" + sub_ent.get("1.0","end-1c") + "\n\n" + mess_ent.get("1.0","end-1c")
+    bsend=Button(master,text="Send Mail",relief="groove",command=lambda: takedet(master,lab,user,to_ent.get(),sub_ent.get("1.0","end-1c"),mess_ent.get("1.0","end-1c")),font="Tahoma 12 bold",fg="#454545",bg="#DFD8DC")
+    bsend.grid(row=4,column=1,pady=(0,20))
 
-    bsend=Button(master,text="Send Message",relief="groove",command=lambda: takedet(user),font="Tahoma 12 bold",fg="#454545",bg="#DFD8DC")
-    bsend.grid(row=3,column=1,columnspan=2,pady=(0,10))
+    attcm=Button(master,text="Add Attachment",relief="groove",command=lambda: flogin(master),font="Tahoma 12 bold",fg="#454545",bg="#DFD8DC")
+    attcm.grid(row=4,column=2,pady=(0,20))
+
+    lgout=Button(master,text="Logout",relief="groove",command=lambda: flogin(master),font="Tahoma 12 bold",fg="#454545",bg="#DFD8DC")
+    lgout.grid(row=4,column=3,pady=(0,20))
 
     master.mainloop()
 
@@ -200,7 +212,6 @@ def dig_log(root):
         log_type=1
         codelogin()
 
-
     elif log_type==1:
         root.destroy()
         ftext="Enter Username And Password"
@@ -208,7 +219,6 @@ def dig_log(root):
         flogin()
 
 
-mess=""
 log_type=0  # 0  -> Normal Login 1 - >Speed Code
 ftext="Enter Username And Password"
 
@@ -224,7 +234,6 @@ def flogin(*args):
     root.wm_title("SIGN IN")
     root.configure(background="#DFD8DC")
     root.geometry("676x220")
-    #animate(1,160,4,1,.005,root)
     var=IntVar()
 
     topframe=Frame(root)
@@ -296,5 +305,5 @@ def codelogin():
 
         login_dig=Button(root,text="Switch To Normal Login",relief=GROOVE,width=25,command=lambda: dig_log(root),font="Tahoma 12 bold",fg="#454545",bg="#DFD8DC")
         login_dig.grid(row=4,column=2,pady=(3,10),padx=200)
-
+#message_box("root","as")
 flogin()
